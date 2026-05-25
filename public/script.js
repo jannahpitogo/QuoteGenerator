da4b1ea815f27e5e6a01500547708fab5005178f
jannahpitogo <pitogojannah@outlook.com>
2026-05-25 02:47:46 +0200

    refactor: update quote submission and retrieval logic in backend and frontend

--- public/script.js
+++ public/script.js
@@ -1,14 +1,43 @@
-function generateQuote() {
+const form = document.getElementById("quote-form");
+
+form.addEventListener("submit", async (event) => {
+  event.preventDefault();
+
   const quote = document.getElementById("quote").value;
   const author = document.getElementById("author").value;
 
-  if (quote.trim() === "" || author.trim() === "") {
-    alert("Please fill in both fields.");
-    return;
+  const res = await fetch("/newquotes", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      quote,
+      author,
+    }),
+  });
+
+  const data = await res.text();
+  console.log(data);
+});
+
+async function newQuote() {
+  try {
+    const response = await fetch("/quote");
+
+    if (!response.ok) throw new Error("Server error");
+
+    const quoteString = await response.text();
+
+    document.getElementById("quote-container").textContent = quoteString;
+  } catch (error) {
+    document.getElementById("quote-container").textContent =
+      "Connection error. Is it running?";
   }
-
-  document.getElementById("displayQuote").innerText = `"${quote}"`;
-  document.getElementById("displayAuthor").innerText = `- ${author}`;
-
-  document.getElementById("quoteBox").style.display = "block";
 }
+
+const newQuoteButton = document.getElementById("new-quote-button");
+newQuoteButton.addEventListener("click", newQuote);
+newQuoteButton.style.cursor = "pointer";
+
+window.onload = newQuote;
